2738997b43abcf891a8c2c8f887efb9ea89e82bc
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-02-18 05:06:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -710,7 +710,7 @@ def start():
                 end_time = times[i][0]
             threading.Thread(target=interval, args=(start_time, start_time + datetime.timedelta(seconds=1), end_time, 0, driver,)).start()
             
-            if (len(sys.argv) == 1 and i == 0) or (len(sys.argv) != 1 and i == 10):
+            if (len(sys.argv) == 1 and i == 0) or (len(sys.argv) != 1 and i == 1 and datetime.datetime.now() < datetime.datetime(start_now.year, start_now.month, start_now.day, 3, 34, 0)):
                 notice(driver)
                 get_334(driver)
                 

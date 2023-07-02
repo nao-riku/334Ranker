e20c89ab02ef69db5432c1bc0b686a699517abb6
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-07-03 02:53:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -572,7 +572,7 @@ def interval(since, until, end, index, driver):
                 threading.Thread(target=interval, args=(until, until + datetime.timedelta(seconds = add), end, index + 1, driver,)).start()
             since2 = since - datetime.timedelta(seconds = 1)
             res = driver.execute_async_script("""
-var url = 'https://api.twitter.com/1.1/search/tweets.json?q=@rank334 since:""" + since2.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ until:""" + until.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ -filter:retweet -filter:quote -from:rank334 -from:rank334_2&count=100&result_type=recent&tweet_mode=extended';
+var url = 'https://api.twitter.com/1.1/search/tweets.json?q=@rank334 since:""" + since2.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ until:""" + until.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ -filter:retweets -filter:quote -from:rank334 -from:rank334_2&count=100&result_type=recent&tweet_mode=extended';
 
 var cookie = document.cookie.replaceAll(" ", "").split(";");
 var token = "";
@@ -1020,7 +1020,7 @@ def get_334(driver):
             driver.execute_script("""
 window.data = "";
 var url1 = 'https://api.twitter.com/1.1/search/';
-var url2 = '.json?count=100&result_type=recent&q=334 since:""" + time1.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ until:""" + time2.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ -filter:retweet -filter:quote -from:rank334 -from:334_Reporter';
+var url2 = '.json?count=100&result_type=recent&q=334 since:""" + time1.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ until:""" + time2.strftime('%Y-%m-%d_%H:%M:%S_JST') + """ -filter:retweets -filter:quote -from:rank334 -from:334_Reporter';
 var out = [];
 var out2 = [];
 var out3 = [];

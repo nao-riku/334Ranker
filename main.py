008d83426fe06e47837c0f75d108b599ab88e8c5
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-07-16 15:49:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1378,7 +1378,7 @@ def start():
             
             get_allresult()
             if len(sys.argv) != 1:
-                start_time = datetime.datetime.now().replace(microsecond = 0) + datetime.timedelta(seconds=2)
+                start_time = datetime.datetime.now().replace(microsecond = 0) + datetime.timedelta(seconds=60)
                 end_time = times[i][0]
             login_twitter("rank334", os.environ['PASS'], os.environ['TEL'], driver)
             login_twitter2("rank334_2", os.environ['PASS'], os.environ['TEL'], driver)

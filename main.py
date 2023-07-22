e416860bddcd39ee397c738ff447edd8724c59cb
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-07-23 08:43:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1564,6 +1564,7 @@ def start():
             options.add_argument("--disable-gpu")
             options.add_argument('--disable-dev-shm-usage')
             driver = webdriver.Chrome(options = options)
+            driver.set_script_timeout(5)
             driver3 = webdriver.Chrome(options = options)
             
         except Exception as e:
